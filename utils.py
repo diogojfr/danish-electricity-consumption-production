8fd3ab6b865a684a4aee473d2a1cafa8562db44f
diogojfr <diogojfr1@gmail.com>
2024-02-10 23:53:51 -0300

    update

--- utils.py
+++ utils.py
@@ -1,15 +1,35 @@
 import psycopg2
 import pandas as pd
-import sqlalchemy
 from sqlalchemy import create_engine
 
-connection_string = ""
-# conn = psycopg2.connect(connection_string)
+connection_string = "postgres://lsprvksb:jf8w1w_ahrthiX2q7oNfb7nKoUnrjoXf@silly.db.elephantsql.com/lsprvksb"
+conn = psycopg2.connect(connection_string)
 
-# conn.set_session(autocommit=True)
-# cur = c
+#conn.set_session(autocommit=True)
+# cur = conn.cursor()
+
+# cur.execute(
+#   """
+#   DROP TABLE IF EXISTS daily_historic_pollution
+#   """)
+
+# cur.execute(
+#   """
+#   CREATE TABLE daily_historic_pollution (date DATE PRIMARY KEY,
+#                          type VARCHAR(20),
+#                          value INTEGER
+#                          )
+#   """)
+# print("Created table")
+#cur.close()
+
+#conn.close()
 # create_engine = asdas
+ 
 engine = create_engine(connection_string)
 
+conn = engine.connect()
 
+df = pd.read_csv("C:/Users/Administrador/Documents/data-science/portifolio-projects/danish-electricity-consumption-and-production/artifacts/raw_tables/raw_data.csv")
 
+df.to_sql('danish_electricity_total', engine)
