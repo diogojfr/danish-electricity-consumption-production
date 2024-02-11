e48253e808f71950bd9c8753d602d438823dc7ef
diogojfr <diogojfr1@gmail.com>
2024-02-10 23:04:56 -0300

    update

---
+++ utils.py
@@ -0,0 +1,15 @@
+import psycopg2
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
+
+connection_string = ""
+# conn = psycopg2.connect(connection_string)
+
+# conn.set_session(autocommit=True)
+# cur = c
+# create_engine = asdas
+engine = create_engine(connection_string)
+
+
+
