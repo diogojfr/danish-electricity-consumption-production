b0ae4ef7c528aa4cb3269b7f132b19b28d2d45ef
diogojfr <diogojfr1@gmail.com>
2024-02-19 18:20:00 -0300

    update

--- utils.py
+++ utils.py
@@ -17,9 +17,34 @@ cur.execute(
 
 cur.execute(
   """
-  CREATE TABLE danish_electricity_total (date DATE PRIMARY KEY,
-                         type VARCHAR(20),
-                         value INTEGER
+  CREATE TABLE danish_electricity_total (HourUTC TIMESTAMP PRIMARY KEY,
+                         HourDK TIMESTAMP,
+                         PriceArea VARCHAR(10),
+                         CentralPowerMWh FLOAT(2), 
+                         LocalPowerMWh FLOAT(2), 
+                         CommercialPowerMWh FLOAT(2), 
+                         LocalPowerSelfConMWh FLOAT(2), 
+                         OffshoreWindLt100MW_MWh FLOAT(2), 
+                         OffshoreWindGe100MW_MWh FLOAT(2), 
+                         OnshoreWindLt50kW_MWh FLOAT(2),
+                         OnshoreWindGe50kW_MWh FLOAT(2), 
+                         HydroPowerMWh FLOAT(2), 
+                         SolarPowerLt10kW_MWh FLOAT(2),
+                         SolarPowerGe10Lt40kW_MWh FLOAT(2), 
+                         SolarPowerGe40kW_MWh FLOAT(2),
+                         SolarPowerSelfConMWh FLOAT(2), 
+                         UnknownProdMWh FLOAT(2), 
+                         ExchangeNO_MWh FLOAT(2),
+                         ExchangeSE_MWh FLOAT(2), 
+                         ExchangeGE_MWh FLOAT(2), 
+                         ExchangeNL_MWh FLOAT(2), 
+                         ExchangeGB_MWh FLOAT(2),
+                         ExchangeGreatBelt_MWh FLOAT(2), 
+                         GrossConsumptionMWh FLOAT(2),
+                         GridLossTransmissionMWh FLOAT(2), 
+                         GridLossInterconnectorsMWh FLOAT(2),
+                         GridLossDistributionMWh FLOAT(2), 
+                         PowerToHeatMWh FLOAT(2)                      
                          )
   """)
 print("Created table")
@@ -38,7 +63,7 @@ for i in range(len(df)):
     ExchangeSE_MWh, ExchangeGE_MWh, ExchangeNL_MWh, ExchangeGB_MWh,
     ExchangeGreatBelt_MWh, GrossConsumptionMWh,
     GridLossTransmissionMWh, GridLossInterconnectorsMWh,
-    GridLossDistributionMWh, PowerToHeatMWh)
-    
+    GridLossDistributionMWh, PowerToHeatMWh) VALUES (%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s);
     """
-
+    engine.execute(query, df.HourUTC[i], df.HourDK[i], df.PriceArea[i],
+                   df.CentralPowerMWh[i], df.LocalPowerMWh[i], df.CommercialPowerMWh[i],df.LocalPowerSelfConMWh[i], )
